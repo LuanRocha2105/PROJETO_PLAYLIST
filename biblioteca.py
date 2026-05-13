1f0b9c288d661be7a9720ae7adcba65a6bdfc749
Luan_Rocha_ <luanv_rocha@hotmail.com>
2026-05-13 10:14:39 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -11,3 +11,17 @@ class Biblioteca:
     def __init__(self):
         self.inicio = None
         self.proximo_id = 1
+
+    def adicionar(self, titulo, artista, genero, bpm):
+        nova_musica = Musica(self.proximo_id, titulo, artista, genero, bpm)
+        self.proximo_id += 1
+        novo_nodo = NodoLista(nova_musica)
+
+        if self.inicio is None:
+            self.inicio = novo_nodo
+        else:
+            atual = self.inicio
+            while atual.proximo is not None:
+                atual = atual.proximo
+            atual.proximo = novo_nodo
+        return nova_musica
